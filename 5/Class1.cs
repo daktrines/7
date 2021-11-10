64619c577ac163d1aa28bad1df6834667ba22a0b
ИСП-31 <-31@LAB17-01>
2021-11-10 12:40:04 +0300

    Добавьте файлы проекта.

---
+++ 5/Class1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _5
+{
+    /// <summary>
+    /// класс машина
+    /// </summary>
+    public class Car
+    {
+        //string _marka;//марка автомобиля (Поля)
+        int _culindr; //количество циллиндров
+        int _moshnost; //мощность
+
+        //метод 
+        public bool Proverka(int value)
+        {
+
+            if (value > 0)
+            {
+                return true;
+            }
+            else return false;
+        }
+        //Свойства
+        public string Marka { get; set; }//марка автомобиля
+        public int Culindr//количество циллиндров
+        {
+            get
+            {
+                return _culindr;
+            }
+            set
+            {
+                if (Proverka(value) == true)
+                {
+                    _culindr = value;
+                }
+                else MessageBox.Show("Неверные данные!", "Ошибка");
+            }
+        }
+        public int Moshnost //мощность
+        {
+            get
+            {
+                return _moshnost;
+            }
+            set
+            {
+                if (Proverka(value) == true)
+                {
+                    _moshnost = value;
+                }
+                else MessageBox.Show("Неверные данные!", "Ошибка");
+            }
+
+        }
+
+        public string Show()
+        {
+            string info = "Машина имеет такую характеристику:\n" +
+                Marka + " - Марка\n" +
+                Culindr + " - Циллиндр\n" +
+                Moshnost + " - Мощность\n";
+            return info;
+
+        }
+
+        public Car() { }
+
+        //Параметризированный конструктор 
+        public Car(string initMarka, int initCulindr, int initMashnost)
+        {
+            Marka = initMarka;
+            Culindr = initCulindr;
+            Moshnost = initMashnost;
+        }
+
+        public Car(string marka)
+        {
+            Marka = marka;
+        }
+
+        //Унарный оператор инкремента 
+        public static Car operator ++(Car moshnost)
+        {
+            moshnost.Moshnost += 1;
+            return moshnost;
+        }
+
+        //Операции сравнения
+        public static bool operator >=(Car onecar, Car twocar)
+        {
+            if (onecar.Moshnost > twocar.Moshnost)
+            {
+                if (onecar.Culindr >= twocar.Culindr) return true;
+
+            }
+            else if (onecar.Moshnost >= twocar.Moshnost)
+            {
+                if (onecar.Culindr > twocar.Culindr) return true;
+            }
+            return false;
+        }
+        //Операции сравнения
+        public static bool operator <=(Car onecar, Car twocar)
+        {
+            if (onecar >= twocar) return false;
+            else return true;
+
+        }
+        //Операции сравнения
+        public static bool operator ==(Car onecar, Car twocar)
+        {
+            if ((onecar.Culindr == twocar.Culindr) && (onecar.Moshnost == twocar.Moshnost)) return true;
+            else return false;
+        }
+        //Операции сравнения
+        public static bool operator !=(Car onecar, Car twocar)
+        {
+            if (onecar == twocar) return false;
+            else return true;
+        }
+
+        //перегруженный метод
+        public void SetParams(string marka)
+        {
+            Marka = marka;
+
+        }
+        //перегруженный метод
+        public void SetParams(int culindr, int moshnost)
+        {
+            Culindr = culindr;
+            Moshnost = moshnost;
+        }
+        //перегруженный метод
+        public void SetParams(string marka, int culindr, int moshnost)
+        {
+            Marka = marka;
+            Culindr = culindr;
+            Moshnost = moshnost;
+        }
+
+        //переопределение из-за ошибки
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+    class Lorry : Car
+    {
+        int _gruz; //грузоподъемность(поле)
+
+        public int Gruz //грузоподъемность
+        {
+            get
+            {
+                return _gruz;
+            }
+            set
+            {
+                if (Proverka(value) == true)
+                {
+                    _gruz = value;
+                }
+                else MessageBox.Show("Неверные данные!", "Ошибка");
+            }
+        }
+
+        public Lorry() { }
+
+        public Lorry(string marka, int gruz) : base(marka)
+        {
+            Gruz = gruz;
+        }
+        public new string Show()
+        {
+            string info = "Грузовик имеет такую характеристику:\n" +
+                Marka + " - Марка\n" +
+                Gruz  + " - Грузоподъемность\n";
+            return info;
+
+        }
+        public int Izmenenie(int gruz)
+        {
+           
+            Gruz = gruz;
+            return gruz;
+        }
+    }
+}
---
+++ 5/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Калион Екатерина Максимовна. 7 пр. Использовать класс Саr (машина), характеризуемый торговой маркой (строка), " +
+                "числом цилиндров, мощностью. Создать производный класс Lorry(грузовик), " +
+                "характеризуемый также грузоподъемностью кузова.Определить функции переназначения марки и изменения грузоподъемности.");
+        }
+
+        //Выход из программы
+        private void Выход_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+     
+       
+        Car car = new Car(); //Создаем обьект типа Car
+        Lorry lorry = new Lorry();
+        Car onecar; //описываем переменную 1 авто
+        Car twocar; //описываем переменную 2 авто
+
+        //покупка первого авто
+        private void Купить_Click(object sender, RoutedEventArgs e)
+        {
+            //преобразования string в int, выходной параметр cilindr / moshonst, "переменная" > 0
+            if (int.TryParse(Cilindr.Text, out int cilindr) && int.TryParse(Moshnost.Text, out int moshnost) && cilindr > 0 && moshnost > 0)
+            {
+                car.SetParams(cilindr, moshnost); // использууем перегруженный метод
+                car1.Text = car.Show(); //используем метод
+                onecar = new Car(car.Marka, car.Moshnost, car.Culindr);// создаем экземпляр обьекта с параметрами
+            }
+            else MessageBox.Show("Введите данные!", "Ошибка");
+        }
+
+        //Радиокнопка бмв
+        private void BMW_Checked(object sender, RoutedEventArgs e)
+        {
+            car.SetParams(Convert.ToString(BMW.Content));// испоьзуем метод для марки
+            Cilindr.Text = "6";
+            Moshnost.Text = "297";
+            car.SetParams(6, 297);
+            
+        }
+
+        //радиокнопка ауди
+        private void Audi_Checked(object sender, RoutedEventArgs e)
+        {
+            car.SetParams(Convert.ToString(Audi.Content));// используем метод
+            Cilindr.Text = "4";
+            Moshnost.Text = "176";
+            car.SetParams(4, 176);
+            
+        }
+
+        //кнопка увеличения на 1
+        private void Plus_Click(object sender, RoutedEventArgs e)
+        {
+            //преобразования string в int, выходной параметр moshonst, "переменная" > 0
+            if ( int.TryParse(Moshnost.Text, out int moshnost) &&  moshnost > 0)
+            {
+                //используем метод +1 
+                car++;
+                Moshnost.Text = Convert.ToString(car.Moshnost) ;
+            }
+            else MessageBox.Show("Введите данные!", "Ошибка");
+        }
+
+        //покупка второго авто
+        private void Купить1_Click(object sender, RoutedEventArgs e)
+        {
+            //преобразования string в int, выходной параметр cilindr / moshonst, "переменная" > 0
+            if (int.TryParse(Cilindr.Text, out int cilindr) && int.TryParse(Moshnost.Text, out int moshnost) && cilindr > 0 && moshnost > 0)
+            {
+                car.SetParams(cilindr, moshnost); // использууем перегруженный метод
+                car2.Text = car.Show(); //используем метод
+                twocar = new Car(car.Marka, car.Moshnost, car.Culindr );// создаем экземпляр обьекта с параметрами
+            }
+            else MessageBox.Show("Введите данные!", "Ошибка");
+        }
+
+        //сравниваем 2 авто
+        private void Сравнить_Click(object sender, RoutedEventArgs e)
+        {
+            //преобразования string в int, выходной параметр cilindr / moshonst, "переменная" > 0
+            if (int.TryParse(Cilindr.Text, out int cilindr) && int.TryParse(Moshnost.Text, out int moshnost) && cilindr > 0 && moshnost > 0)
+            {
+                if (onecar == twocar) srav.Text = "Оба авто крутые";
+                else if (onecar >= twocar) srav.Text = "BMW круче";
+                else srav.Text = "Audi круче";
+            }
+            else MessageBox.Show("Введите данные!", "Ошибка");
+        }
+
+        //Очищаем все
+        private void Очистить_Click(object sender, RoutedEventArgs e)
+        {
+            
+            Cilindr.Clear();
+            Moshnost.Clear();
+            car1.Clear();
+            car2.Clear();
+            srav.Clear();
+            Gruz.Clear();
+            Lorry.Clear();
+
+        }
+
+        //радиокнопка волва
+        private void Volvo_Checked(object sender, RoutedEventArgs e)
+        {
+            lorry.SetParams(Convert.ToString(Volvo.Content));// используем метод
+            Gruz.Text = "60";
+            Lorry.Clear();
+        }
+
+        //радиокнопка хино
+        private void HINO_Checked(object sender, RoutedEventArgs e)
+        {
+            lorry.SetParams(Convert.ToString(HINO.Content));// используем метод
+            Gruz.Text = "26";
+            Lorry.Clear();
+        }
+
+        //кнопка купить
+        private void КупитьГрузовик_Click(object sender, RoutedEventArgs e)
+        {
+            //преобразования string в int, выходной параметр gruz, "переменная" > 0
+            if (int.TryParse(Gruz.Text, out int gruz) && gruz > 0)
+            {
+                lorry.Izmenenie(gruz); // использууем функцию
+                Lorry.Text = lorry.Show(); //используем метод
+            }
+            else MessageBox.Show("Введите данные!", "Ошибка");
+        }
+    }
+}
+       
+    
+
